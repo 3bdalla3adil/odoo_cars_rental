dfdecba323289d87a5f2ce1eb44c6ad2ed0842f3
3bdalla3adil <abdullagozer@gmail.com>
2023-09-13 13:36:25 +0300

    modified  price to be 100AED

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-# from datetime import datetime
-
 from odoo import api, fields, models, _
 
 
@@ -18,7 +16,7 @@ class CarsRental(models.Model):
     _inherit = "client.info"
     
     price            = fields.Monetary(string='Price',compute="_compute_price", currency_field='currency_id')
-    currency_id      = fields.Many2one('res.currency',readonly=True,default='AED')
+    currency_id      = fields.Many2one('res.currency',readonly=True)
     distance_per_day = fields.Integer(string="distance_per_day")
 
     # to set the currency to be 'AED' 
@@ -36,40 +34,39 @@ class CarsRental(models.Model):
         # set currency_id field to 'AED' id
         self.currency_id = currency
 
+    def check_exceeded_distance(self,distance_per_day,price):
+        if distance_per_day > 200:
+            # calculate the extra kilometers as 10 AED Fine 
+            extra_km_fine = int(distance_per_day - 200) * 10
+            # and add the Fine to the price
+            price += extra_km_fine
+
     @api.depends( "rent_start_period", "rent_end_period")
     def _compute_price(self):
+        
         for record in self:
+
             # chech if user set starting period and ending period
             if  record.rent_start_period and record.rent_end_period:
                 # calculate price upon the start date and end date 
                 # assuming the customer is able to rent at any period of time
-                record.price = 200 * int((record.rent_end_period - record.rent_start_period).days)
+                record.price = 100 * int((record.rent_end_period - record.rent_start_period).days)
                 
                 # check if the distance exceeded 200 KM
-                if record.distance_per_day > 200:
-                    # calculate the extra kilometers as 10 AED Fine 
-                    extra_km_fine = int(record.distance_per_day - 200) * 10
-                    # and add the Fine to the price
-                    record.price += extra_km_fine
+                self.check_exceed_distance(record.distance_per_day,record.price)
 
             elif  not record.rent_start_period and record.rent_end_period:
                 # assuming the starting period and ending period is the time when user saved the record 
                 record.rent_start_period = fields.Datetime.today()
                 # count the deifference between starting and ending date to calculate price over the renting period
-                record.price = 200 * int((record.rent_start_period - record.rent_end_period).days)
+                record.price = 100 * int((record.rent_start_period - record.rent_end_period).days)
                 # check if the distance exceeded the 200 KM
-                if record.distance_per_day > 200:
-                    # calculate the extra kilometers as 10 AED Fine 
-                    extra_km_fine = int(record.distance_per_day - 200) * 10
-                    # and add the Fine to the price
-                    record.price += extra_km_fine
-            # if niether  starting nor ending rent period set
+                self.check_exceed_distance(record.distance_per_day,record.price)
+                
+            # if niether starting nor ending rent period set
             else:
+                # check if the distance exceeded 200 KM
+                self.check_exceed_distance(record.distance_per_day,record.price)
+
                 # set price as zero
-                record.price = 0
-
-
-
-
-
-
+                record.price = 100
