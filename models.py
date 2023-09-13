7f208956ecdd8933f63587a27fd72ff5f75cd0fe
3bdalla3adil <abdullagozer@gmail.com>
2023-09-13 11:42:53 +0300

    fixed fine problem

--- models.py
+++ models.py
@@ -17,18 +17,11 @@ class CarsRental(models.Model):
     _name = "cars.rental"
     _inherit = "client.info"
 
-    # name    = fields.Char(string="Name", required=True)
-    # address = fields.Char('Address')
-
-    # 200    = fields.Float(string='Day Price', digits=(16, 0))
-    # start_period = fields.Date(string="Start Date", tracking=1)
-    # end_period   = fields.Date(string="Received Date", tracking=1)
     currency_id  = fields.Many2one('res.currency',readonly=True, 
                     default=lambda self: self.env['res.currency'].search([('name', '=', 'AED')], limit=1))
     price        = fields.Monetary(string='Price',compute="_compute_price", currency_field='currency_id')
     
-    distance_per_day = fields.Integer(string="KM")
-    # fine = fields.Float(string="Fine")
+    distance_per_day = fields.Integer(string="distance_per_day")
 
 
     #Adds Interactive Calculation Feature
@@ -36,7 +29,7 @@ class CarsRental(models.Model):
     # def _get_price(self):
     #     if self.distance_per_day > 200:
 
-    #         # Assuming that we our client rent cars for a 100 AED/day and Add 10 AED fine
+    #         # Assuming that we our client rent cars for a 100 AED/day and Add Fine AED 10
     #         return 100 + int( ( self.distance_per_day - 200 ) * 10 )
     #     else:
     #         return 100
@@ -46,15 +39,15 @@ class CarsRental(models.Model):
         for rec in self:
             if  rec.rent_start_period and rec.rent_end_period:
                 rec.price = 200 * float((rec.rent_end_period - rec.rent_start_period).days)
-                if rec.km > 200:
-                    var = (rec.km - 200) * rec.fine
+                if rec.distance_per_day > 200:
+                    var = (rec.distance_per_day - 200) * 10
                     rec.price += var
 
             elif  not rec.rent_start_period and rec.rent_end_period:
                 rec.rent_start_period = fields.Datetime.today()
                 rec.price = 200 * float((rec.rent_start_period - rec.rent_end_period).days)
-                if rec.km > 200:
-                    var = (rec.km - 200) * 10
+                if rec.distance_per_day > 200:
+                    var = (rec.distance_per_day - 200) * 10
                     rec.price += var
 
             else:
