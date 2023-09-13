f0937243a7119beb6e2bce2eea8131365139f498
3bdalla3adil <abdullagozer@gmail.com>
2023-09-13 11:30:09 +0300

    commented _get_price and uncommented old function

--- models.py
+++ models.py
@@ -25,40 +25,40 @@ class CarsRental(models.Model):
     # end_period   = fields.Date(string="Received Date", tracking=1)
     currency_id  = fields.Many2one('res.currency',readonly=True, 
                     default=lambda self: self.env['res.currency'].search([('name', '=', 'AED')], limit=1))
-    price        = fields.Monetary(string='Price',compute="_get_price", currency_field='currency_id')
+    price        = fields.Monetary(string='Price',compute="_compute_price", currency_field='currency_id')
     
     distance_per_day = fields.Integer(string="KM")
     # fine = fields.Float(string="Fine")
 
 
     #Adds Interactive Calculation Feature
-    @api.depends('distance_per_day')
-    def _get_price(self):
-        if self.distance_per_day > 200:
+    # @api.depends('distance_per_day')
+    # def _get_price(self):
+    #     if self.distance_per_day > 200:
 
-            # Assuming that we our client rent cars for a 100 AED/day and Add 10 AED fine
-            return 100 + int( ( self.distance_per_day - 200 ) * 10 )
-        else:
-            return 100
+    #         # Assuming that we our client rent cars for a 100 AED/day and Add 10 AED fine
+    #         return 100 + int( ( self.distance_per_day - 200 ) * 10 )
+    #     else:
+    #         return 100
         
-    # @api.depends("day_price", "start_date", "end_date", "fine")
-    # def _compute_price(self):
-    #     for rec in self:
-    #         if rec.day_price and rec.start_date and rec.end_date:
-    #             rec.price = rec.day_price * float((rec.end_date - rec.start_date).days)
-    #             if rec.km > 200:
-    #                 var = (rec.km - 200) * rec.fine
-    #                 rec.price += var
+    @api.depends("day_price", "start_date", "end_date")
+    def _compute_price(self):
+        for rec in self:
+            if rec.day_price and rec.start_date and rec.end_date:
+                rec.price = rec.day_price * float((rec.end_date - rec.start_date).days)
+                if rec.km > 200:
+                    var = (rec.km - 200) * rec.fine
+                    rec.price += var
 
-    #         elif rec.day_price and not rec.start_date and rec.end_date:
-    #             rec.start_date = fields.Datetime.today()
-    #             rec.price = rec.day_price * float((rec.start_date - rec.end_date).days)
-    #             if rec.km > 200:
-    #                 var = (rec.km - 200) * rec.fine
-    #                 rec.price += var
+            elif rec.day_price and not rec.start_date and rec.end_date:
+                rec.start_date = fields.Datetime.today()
+                rec.price = rec.day_price * float((rec.start_date - rec.end_date).days)
+                if rec.km > 200:
+                    var = (rec.km - 200) * rec.fine
+                    rec.price += var
 
-    #         else:
-    #             rec.price = 0
+            else:
+                rec.price = 0
 
 
 
